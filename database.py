d4816d4131d44df896a9773c92e1d2d8aeefe857
prathameshkarve01 <prathameshkarve01@gmail.com>
2026-07-13 00:21:44 +0530

    Update DATABASE_URL to point to the production database location

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./students.db"
+#DATABASE_URL = "sqlite:///./students.db"
+DATABASE_URL = "sqlite:////opt/student-management/students.db"
 
 engine = create_engine(
     DATABASE_URL,
